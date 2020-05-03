bc2961b438c7e1af486b865bb918873d90778796
Olubiyi Emmanuel <42422184+coded1guy@users.noreply.github.com>
2020-05-03 23:20:21 +0100

    Delete index.js

--- index.js
+++
@@ -1,6 +0,0 @@
-function calcBMI() {
-	var height = Number(document.getElementById('height').value);
-	var weight = Number(document.getElementById('weight').value);
-	var result = weight / height * height;
-	document.getElementById("BMI_result").innerHTML = "Your BMI is  " + result.toFixed(2);
-};
